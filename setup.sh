906756b1470d322c363d7fc8ca0f30f2c1d930d7
Mark E. Schill <Mark.Schill@cmschill.net>
2022-04-09 16:32:09 -0400

    updates

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 cd "$HOME" || return
 system_type=$(uname -s)
 
-if [ $system_type -eq "Darwin" ]; then
+if [[ $system_type == "Darwin" ]]; then
   echo "Hello Mac User!"
   echo "Installing Hombebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -15,10 +15,11 @@ if [ $system_type -eq "Darwin" ]; then
   # Install Chezmoi
   brew install chezmoi
 
-elif [ $system_type -eq "Linux"]; then
+elif [[ $system_type == "Linux" ]]; then
+
     distribution=$(lsb_release -i | cut -f 2-)
 
-    if [ $distribution -eq "Ubuntu" ]; then
+    if [[ $distribution == "Ubuntu" ]]; then
         sudo apt-get update
         sudo apt-get -y install git curl
 
@@ -27,9 +28,12 @@ elif [ $system_type -eq "Linux"]; then
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
         sudo apt update
         sudo apt install gh
-    else
-        sudo yum update
+    elif [[ $distribution == "Fedora" ]]; then
         sudo yum -y install git
+
+        sudo dnf install 'dnf-command(config-manager)'
+        sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+        sudo dnf -y install gh
     fi
 
     # Install chezmoi
