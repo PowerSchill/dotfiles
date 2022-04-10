5928ef653b008d8566b3480f979262c18c6700f4
Mark E. Schill <Mark.Schill@cmschill.net>
2022-04-10 14:27:18 -0400

    Script cleanup

--- home/.chezmoitemplates/Microsoft.PowerShell_profile.ps1
+++ home/.chezmoitemplates/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,19 @@
 # This is my profile
 
+# Set Path
+if ( $IsMacOS ) {
+    $Platform = (uname -m)
+
+    switch ($Platform) {
+        'arm64' {
+           $Env:PATH += ":/opt/homebrew/bin" 
+        }
+    }
+}
+
+
+# Add local bin to PATH
+$Env:PATH += ':~/bin'
+
 # Run Oh-my-posh
 oh-my-posh init pwsh --config ~/.oh-my-posh.json | Invoke-Expression
