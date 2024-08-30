c84b5fda30cd4f4fdfdb17ffb60b53e3493d0592
Mark E. Schill <Mark.Schill@cmschill.net>
2024-08-30 17:06:23 -0400

    chore: working with Windows

--- install.ps1
+++ install.ps1
@@ -1,1 +1,7 @@
 winget install twpayne.chezmoi
+winget install --id GitHub.cli
+winget install Git.Git
+winget install AgileBits.1Password
+winget install AgileBits.1Password.CLI
+
+chezmoi init --apply --verbose git@github.com:PowerSchill/dotfiles.git
