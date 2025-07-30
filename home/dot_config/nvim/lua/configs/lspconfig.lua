668fa78ec526d13979e33926f5b8a42a158c81ba
Mark E. Schill <Mark.Schill@candescent.com>
2025-07-30 09:01:47 -0400

    Remove .config/nvim/README.md Remove .config/nvim/.stylua.toml Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lua/autocmds.lua Remove .config/nvim/lua/chadrc.lua Remove .config/nvim/lua/configs/conform.lua Remove .config/nvim/lua/configs/lazy.lua Remove .config/nvim/lua/configs/lspconfig.lua Remove .config/nvim/lua/mappings.lua Remove .config/nvim/lua/options.lua Remove .config/nvim/lua/plugins/init.lua

--- home/dot_config/nvim/lua/configs/lspconfig.lua
+++
@@ -1,8 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "html", "cssls"}
-vim.lsp.enable(servers)
-
-vim.lsp.enable('terraformls')
-
--- read :h vim.lsp.config for changing options of lsp servers 
