7f020f94b7a3dbed21f25b5fc48b1636cf73f225
Mark E. Schill <Mark.Schill@cmschill.net>
2025-08-16 19:50:18 -0400

    Updates

--- home/dot_config/wezterm/config/appearance.lua
+++ home/dot_config/wezterm/config/appearance.lua
@@ -1,51 +1,37 @@
 return function(config, wezterm)
-	local act = wezterm.action
+  local act = wezterm.action
 
-	local function is_dark()
-		if wezterm.gui then
-			local overrides = wezterm.gui.get_appearance()
-			return overrides:find("Dark") ~= nil
-		else
-			return false
-		end
-	end
+  -- Color
+  config.color_scheme = "Catppuccin Mocha"
+  config.hide_tab_bar_if_only_one_tab = true
+  config.window_decorations = "RESIZE" -- "TITLE | RESIZE" is the default
+  config.window_padding = {
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
+  }
+  config.macos_window_background_blur = 30
 
-	-- Color
-	if is_dark() then
-		config.color_scheme = "Catppuccin Mocha"
-	else
-		config.color_scheme = "Catppuccin Mocha" -- will enable this when more of my configurations support light mode
-	end
+  -- Miscellaneous settings
+  config.max_fps = 120
+  config.prefer_egl = true
 
-	config.hide_tab_bar_if_only_one_tab = true
-	config.window_decorations = "RESIZE" -- "TITLE | RESIZE" is the default
-	config.window_padding = {
-		left = 10,
-		right = 10,
-		top = 10,
-		bottom = 10,
-	}
-	config.macos_window_background_blur = 30
-
-	-- Miscellaneous settings
-	config.max_fps = 120
-	config.prefer_egl = true
-
-	wezterm.on("augment-command-palette", function(window)
-		return {
-			{
-				brief = "Toogle terminal transparency",
-				icon = "md_circle_opacity",
-				action = wezterm.action_callback(function()
-					local overrides = window:get_config_overrides() or {}
-					if overrides.window_background_opacity == nil then
-						overrides.window_background_opacity = 0.7
-					else
-						overrides.window_background_opacity = nil
-					end
-					window:set_config_overrides(overrides)
-				end),
-			},
-		}
-	end)
+  wezterm.on("augment-command-palette", function(window)
+    return {
+      {
+        brief = "Toogle terminal transparency",
+        icon = "md_circle_opacity",
+        action = wezterm.action_callback(function()
+          local overrides = window:get_config_overrides() or {}
+          if overrides.window_background_opacity == nil then
+            overrides.window_background_opacity = 0.7
+          else
+            overrides.window_background_opacity = nil
+          end
+          window:set_config_overrides(overrides)
+        end),
+      },
+    }
+  end)
 end
--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,9 +1,10 @@
 local config = require("config")({
-	require("config.appearance"),
-	require("config.bindings"),
-	require("config.fonts"),
-	require("config.general"),
-	require("commands"),
+  require("config.appearance"),
+  require("config.bindings"),
+  require("config.fonts"),
+  require("config.general"),
+  require("config.hyperlinks"),
+  require("commands"),
 }).config
 
 return config
