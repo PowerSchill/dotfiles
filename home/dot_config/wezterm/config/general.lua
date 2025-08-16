193f3f75023632c4aacc2b1908fb1a821f8d3e25
Mark E. Schill <Mark.Schill@cmschill.net>
2025-08-15 22:13:43 -0400

    Update .config/wezterm/config/general.lua

--- home/dot_config/wezterm/config/general.lua
+++ home/dot_config/wezterm/config/general.lua
@@ -1,1 +1,18 @@
-return function(config, wezterm) end
+return function(config, wezterm)
+	config.exit_behavior = "CloseOnCleanExit"
+	config.scrollback_lines = 5000
+
+	config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+	table.insert(config.hyperlink_rules, {
+		regex = [[["']?([\w\d]{1}[-\w\d]+)(/){1}([-w\d\.]+)["']?]],
+		format = "https://www.github.com/$1/$3",
+	})
+
+	config.audible_bell = "Disabled"
+	config.visual_bell = {
+		fade_in_duration_ms = 75,
+		fade_out_duration_ms = 75,
+		target = "CursorColor",
+	}
+end
