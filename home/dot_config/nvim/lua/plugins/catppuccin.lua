f7d4d41dbf095acfedcdcba7cf5a63f863916c58
Mark E. Schill <Mark.Schill@candescent.com>
2025-08-26 14:23:11 -0400

    fix(nvim): fixes compatibility issue with new catppuccin version

--- home/dot_config/nvim/lua/plugins/catppuccin.lua
+++
@@ -1,6 +0,0 @@
--- catppuccin
--- https://github.com/catppuccin/nvim
-
-return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-}
--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,46 +1,15 @@
 return {
-  { -- was missing this
+  {
     "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      transparent_background = true,
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-  }, -- and this
+    opts = function(_, opts)
+      -- Required until https://github.com/LazyVim/LazyVim/pull/6354 is merged.
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
