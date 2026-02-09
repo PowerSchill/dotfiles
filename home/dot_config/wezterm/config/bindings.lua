57ae66c4a86d8b88075110d3286ea4584760e020
Mark E. Schill <Mark.Schill@cmschill.net>
2026-02-08 19:12:44 -0500

    Update .claude/settings.json Update .config/wezterm/config/bindings.lua

--- home/dot_config/wezterm/config/bindings.lua
+++ home/dot_config/wezterm/config/bindings.lua
@@ -1,30 +1,30 @@
 return function(config, wezterm)
-	local act = wezterm.action
-	config.keys = {
-		{
-			key = "t",
-			mods = "CMD|SHIFT",
-			action = act.ShowTabNavigator,
-		},
-		{
-			key = "R",
-			mods = "CMD|SHIFT",
-			action = act.PromptInputLine({
-				description = "Enter new name for tab",
-				action = wezterm.action_callback(function(window, _, line)
-					-- line will be `nil` if they hit escape without entering anything
-					-- An empty string if they just hit enter
-					-- Or the actual line of text they wrote
-					if line then
-						window:active_tab():set_title(line)
-					end
-				end),
-			}),
-		},
-		{
-			key = "p",
-			mods = "CMD|SHIFT",
-			action = act.ActivateCommandPalette,
-		},
-	}
+  local act = wezterm.action
+  config.keys = {
+    {
+      key = "t",
+      mods = "CMD|SHIFT",
+      action = act.ShowTabNavigator,
+    },
+    {
+      key = "R",
+      mods = "CMD|SHIFT",
+      action = act.PromptInputLine({
+        description = "Enter new name for tab",
+        action = wezterm.action_callback(function(window, _, line)
+          -- line will be `nil` if they hit escape without entering anything
+          -- An empty string if they just hit enter
+          -- Or the actual line of text they wrote
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      }),
+    },
+    {
+      key = "p",
+      mods = "CMD|SHIFT",
+      action = act.ActivateCommandPalette,
+    },
+  }
 end
