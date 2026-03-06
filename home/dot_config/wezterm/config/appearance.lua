c36b5d8083c4a52411329badaeee3291b78f8856
Mark E. Schill <Mark.Schill@cmschill.net>
2026-03-06 06:56:11 -0500

    Update .config/wezterm/config/appearance.lua

--- home/dot_config/wezterm/config/appearance.lua
+++ home/dot_config/wezterm/config/appearance.lua
@@ -1,20 +1,48 @@
 return function(config, wezterm)
   local act = wezterm.action
 
-  -- Color
-  -- config.color_scheme = "Catppuccin Latte"
+  -- Catppuccin Mocha palette
+  local mocha = {
+    crust    = '#11111b',
+    mantle   = '#181825',
+    base     = '#1e1e2e',
+    surface0 = '#313244',
+    surface1 = '#45475a',
+    surface2 = '#585b70',
+    overlay0 = '#6c7086',
+    overlay1 = '#7f849c',
+    subtext0 = '#a6adc8',
+    subtext1 = '#bac2de',
+    text     = '#cdd6f4',
+    lavender = '#b4befe',
+    blue     = '#89b4fa',
+    sapphire = '#74c7ec',
+    sky      = '#89dceb',
+    teal     = '#94e2d5',
+    green    = '#a6e3a1',
+    yellow   = '#f9e2af',
+    peach    = '#fab387',
+    maroon   = '#eba0ac',
+    red      = '#f38ba8',
+    mauve    = '#cba6f7',
+    pink     = '#f5c2e7',
+    flamingo = '#f2cdcd',
+    rosewater = '#f5e0dc',
+  }
+
+  -- Color scheme
   config.color_scheme = "Catppuccin Mocha"
 
   local bg_background = os.getenv("HOME") .. "/.config/wezterm/assets/Wezterm Background Blurred.png"
   config.window_background_image = bg_background
 
-  config.hide_tab_bar_if_only_one_tab = true
-  config.use_fancy_tab_bar = true
-  config.window_decorations = "RESIZE" -- "TITLE | RESIZE" is the default
-  config.window_frame = {
-    font = wezterm.font({ family = 'Berkeley Mono', weight = 'Bold' }),
-    font_size = 11,
-  }
+  -- Tab bar
+  config.hide_tab_bar_if_only_one_tab = false
+  config.use_fancy_tab_bar = false
+  config.show_new_tab_button_in_tab_bar = false
+  config.tab_bar_at_bottom = false
+  config.tab_max_width = 32
+  config.window_decorations = "RESIZE"
   config.macos_window_background_blur = 30
   config.window_background_opacity = 0.9
 
@@ -29,30 +57,94 @@ return function(config, wezterm)
   config.max_fps = 120
   config.prefer_egl = true
 
-  -- Configure command palette appearance
-  config.command_palette_bg_color = "#1e1e2e"
-  config.command_palette_fg_color = "#cdd6f4"
+  -- Tab bar colors
+  config.colors = {
+    tab_bar = {
+      background = mocha.crust,
+      active_tab = {
+        bg_color = mocha.base,
+        fg_color = mocha.lavender,
+        intensity = 'Bold',
+      },
+      inactive_tab = {
+        bg_color = mocha.crust,
+        fg_color = mocha.overlay1,
+      },
+      inactive_tab_hover = {
+        bg_color = mocha.surface0,
+        fg_color = mocha.text,
+      },
+      new_tab = {
+        bg_color = mocha.crust,
+        fg_color = mocha.overlay0,
+      },
+      new_tab_hover = {
+        bg_color = mocha.surface0,
+        fg_color = mocha.text,
+      },
+    },
+  }
 
+  -- Command palette appearance
+  config.command_palette_bg_color = mocha.base
+  config.command_palette_fg_color = mocha.text
+
+  -- Tab title helper
+  local function tab_title(tab_info)
+    local title = tab_info.tab_title
+    if title and #title > 0 then
+      return title
+    end
+    return tab_info.active_pane.title
+  end
+
+  -- Half-circle separators for rounded tab look
+  local LEFT_CIRCLE = wezterm.nerdfonts.ple_left_half_circle_thick
+  local RIGHT_CIRCLE = wezterm.nerdfonts.ple_right_half_circle_thick
+
+  wezterm.on('format-tab-title', function(tab, tabs, panes, _config, hover, max_width)
+    local tab_bg = mocha.crust
+    local tab_fg = mocha.overlay1
+    local edge_bg = mocha.crust
+
+    if tab.is_active then
+      tab_bg = mocha.base
+      tab_fg = mocha.lavender
+    elseif hover then
+      tab_bg = mocha.surface0
+      tab_fg = mocha.text
+    end
+
+    local title = tab_title(tab)
+    local index = tab.tab_index + 1
+
+    -- Truncate title to fit
+    title = wezterm.truncate_right(title, max_width - 6)
+
+    return {
+      { Background = { Color = edge_bg } },
+      { Foreground = { Color = tab_bg } },
+      { Text = LEFT_CIRCLE },
+      { Background = { Color = tab_bg } },
+      { Foreground = { Color = tab_fg } },
+      { Attribute = { Intensity = tab.is_active and 'Bold' or 'Normal' } },
+      { Text = ' ' .. index .. ': ' .. title .. ' ' },
+      { Background = { Color = edge_bg } },
+      { Foreground = { Color = tab_bg } },
+      { Text = RIGHT_CIRCLE },
+    }
+  end)
+
+  -- Right status with hostname
   wezterm.on('update-status', function(window)
-    -- Grab the utf8 character for the "powerline" left facing
-    -- solid arrow.
-    local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-
-    -- Grab the current window's configuration, and from it the
-    -- palette (this is the combination of your chosen colour scheme
-    -- including any overrides).
-    local color_scheme = window:effective_config().resolved_palette
-    local bg = color_scheme.background
-    local fg = color_scheme.foreground
+    local LEFT_ARROW = wezterm.nerdfonts.ple_left_half_circle_thick
 
     window:set_right_status(wezterm.format({
-      -- First, we draw the arrow...
-      { Background = { Color = 'none' } },
-      { Foreground = { Color = bg } },
-      { Text = SOLID_LEFT_ARROW },
-      -- Then we draw our text
-      { Background = { Color = bg } },
-      { Foreground = { Color = fg } },
+      { Background = { Color = mocha.crust } },
+      { Foreground = { Color = mocha.surface0 } },
+      { Text = LEFT_ARROW },
+      { Background = { Color = mocha.surface0 } },
+      { Foreground = { Color = mocha.subtext0 } },
       { Text = ' ' .. wezterm.hostname() .. ' ' },
     }))
   end)
